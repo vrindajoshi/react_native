22411fb7d41e43d1fd50d5d85a481a2307cafa13
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-07-21 22:32:37 -0400

    base

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+ // rnfes (shortcut)
+const Home = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
