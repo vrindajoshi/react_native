361756bf48eae57f9205923e74d98a7b78947bda
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-07-22 22:16:32 -0400

    expo router stack

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -8,7 +8,7 @@ const RootLayout = () => {
       <Stack>
         <Stack.Screen name="index" options={{title: 'Home'}}/>
         <Stack.Screen name="about" options={{title: 'About'}}/>
-        <Stack.Screen name="contact" options={{title: 'Contact'}}/>
+        <Stack.Screen name="contact" options={{title: 'Contact', headerShown: false}}/>
       </Stack>
       <Text>Footer</Text> 
     </View>
