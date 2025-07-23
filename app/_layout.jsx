4f412ec4f60901878a1f8e8c0f953168ca85e2bd
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-07-22 22:19:04 -0400

    formatting

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,14 +4,14 @@ import { Slot, Stack } from 'expo-router'
 
 const RootLayout = () => {
   return (
-    <View style={{flex:1}}>
-      <Stack>
+      <Stack screenOptions={{
+        headerStyle: {backgroundColor: "#ddd"},
+        headerTintColor: '#333',
+      }}>
         <Stack.Screen name="index" options={{title: 'Home'}}/>
         <Stack.Screen name="about" options={{title: 'About'}}/>
         <Stack.Screen name="contact" options={{title: 'Contact', headerShown: false}}/>
       </Stack>
-      <Text>Footer</Text> 
-    </View>
   )
 }
 
