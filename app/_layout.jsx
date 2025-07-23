d7ade5a9c59a7590f8776b92badc6ca213460b72
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-07-23 19:24:46 -0400

    light and dark?

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, useColorScheme, View } from 'react-native'
 import React from 'react'
 import { Slot, Stack } from 'expo-router'
+import {Colors} from "../constants/Colors"
 
 const RootLayout = () => {
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
+  
   return (
       <Stack screenOptions={{
-        headerStyle: {backgroundColor: "#ddd"},
-        headerTintColor: '#333',
+        headerStyle: {backgroundColor: theme.navBackground},
+        headerTintColor: theme.title,
       }}>
         <Stack.Screen name="index" options={{title: 'Home'}}/>
         <Stack.Screen name="about" options={{title: 'About'}}/>
