9387bc56cc92cae2c4be462e42232ac83803cc80
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-07-22 22:15:42 -0400

    expo router stack

---
+++ app/_layout.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Slot, Stack } from 'expo-router'
+
+const RootLayout = () => {
+  return (
+    <View style={{flex:1}}>
+      <Stack>
+        <Stack.Screen name="index" options={{title: 'Home'}}/>
+        <Stack.Screen name="about" options={{title: 'About'}}/>
+        <Stack.Screen name="contact" options={{title: 'Contact'}}/>
+      </Stack>
+      <Text>Footer</Text> 
+    </View>
+  )
+}
+
+export default RootLayout
+
+const styles = StyleSheet.create({})
+
+/* Stack uses FiLo to navigate through the most recently used pages */
