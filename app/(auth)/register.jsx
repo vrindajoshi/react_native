389197672cdb96aa9d868d4fe3f655853c41dc5b
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-09-26 16:41:47 -0400

    ThemedButton

--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,11 +1,19 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import { Link } from 'expo-router'
+import { Colors } from '../../constants/Colors'
 
+// themed components
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
 import Spacer from '../../components/Spacer'
+import ThemedButton from '../../components/ThemedButton'
 
 const Register = () => {
+
+  const handleSubmit = () => {
+    console.log('register form submitted')
+  }
+
   return (
     <ThemedView style={styles.container}>
 
@@ -13,6 +21,12 @@ const Register = () => {
       <ThemedText title={true} style={styles.title}>
         Register an Account
       </ThemedText>
+
+      <ThemedButton
+        onPress={handleSubmit}>
+          <Text style={{color:'#f2f2f2'}}>Register</Text>
+
+      </ThemedButton>
 
       <Spacer height={100} />
       <Link href="/login" replace>
@@ -29,12 +43,21 @@ export default Register
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  title: {
-    textAlign: "center",
-    fontSize: 18,
-    marginBottom: 30
-  },
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    title: {
+      textAlign: "center",
+      fontSize: 18,
+      marginBottom: 30
+    },
+    btn: {
+      backgroundColor: Colors.primary,
+      padding: 15,
+      borderRadius: 5,
+    },
+    pressed: {
+      opacity: 0.8
+    }
 })
